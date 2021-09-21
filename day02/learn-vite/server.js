3fe7b8f2654549d6cb9d78fbf051dad95162ddab
Colin3191 <319151881@qq.com>
2021-09-21 23:40:40 +0800

    feature:mini-vite

---
+++ day02/learn-vite/server.js
@@ -0,0 +1,129 @@
+const Koa = require("koa");
+const app = new Koa();
+
+const fs = require("fs");
+const path = require("path");
+
+const { parse } = require("@vue/compiler-sfc");
+const { compile } = require("@vue/compiler-dom");
+
+function rewriteImport(content) {
+  return content
+    .replace(/(from\s+['"])(?![\.\/])/g, "$1/@modules/")
+    .replace(/process\.env\.NODE_ENV/g, '"development"');
+}
+
+app.use((ctx) => {
+  const url = ctx.request.url;
+  console.log(url);
+  if (url === "/") {
+    ctx.body = fs.readFileSync("./index.html", "utf-8");
+  } else if (url.endsWith(".js")) {
+    console.log(url.slice(1));
+    const p = path.resolve(__dirname, url.slice(1));
+    console.log(p);
+    ctx.type = "text/javascript";
+    const source = fs.readFileSync(p, "utf-8");
+    ctx.body = rewriteImport(source);
+  } else if (url.startsWith("/@modules")) {
+    const moduleName = url.replace("@modules", "");
+    const prefix = __dirname + "/node_modules" + moduleName;
+    const module = require(prefix + "/package.json").module;
+    const code = fs.readFileSync(path.resolve(prefix, module), "utf-8");
+    ctx.type = "text/javascript";
+    ctx.body = rewriteImport(code);
+  } else if (url.endsWith(".vue")) {
+    const p = path.resolve(__dirname, url.slice(1));
+    const ret = parse(fs.readFileSync(p, "utf-8"));
+    console.log("ret", ret);
+    // 获取vue文件中的script部分,并将默认导出的部分转换为常量
+    const scriptContent = ret.descriptor.script.content;
+    const script = scriptContent.replace(
+      "export default ",
+      "const __script = "
+    );
+    ctx.type = "text/javascript";
+    // 拼接render
+    ctx.body = `
+      ${rewriteImport(script)}
+      import { render as __render } from '${url}?type=template'
+      __script.render = __render
+      export default __script
+    `;
+  } else if (url.endsWith("?type=template")) {
+    const p = path.resolve(__dirname, url.split("?")[0].slice(1));
+    const ret = parse(fs.readFileSync(p, "utf-8"));
+    const template = ret.descriptor.template.content;
+    console.log("template", template);
+    const render = compile(template, { mode: "module" }).code;
+    console.log("render", render);
+    ctx.type = "text/javascript";
+    ctx.body = rewriteImport(render);
+  } else if (url.endsWith(".png")) {
+    ctx.body = fs.readFileSync("src" + url);
+  }
+});
+
+// app.use(async (ctx) => {
+//   // ctx.body = 'kkb vite'
+//   const url = ctx.request.url;
+//   if (url === "/") {
+//     ctx.type = "text/html";
+//     console.log(ctx.path)
+//     ctx.body = fs.readFileSync("./index.html", "utf-8");
+//   } else if (url.endsWith(".js")) {
+//     const p = path.resolve(__dirname, url.slice(1));
+//     ctx.type = "text/javascript";
+//     const ret = fs.readFileSync(p, "utf-8");
+//     ctx.body = rewriteImport(ret);
+//   } else if (url.startsWith("/@modules")) {
+//     const moduleName = url.replace("/@modules/", "");
+//     const prefix = path.resolve(__dirname, "node_modules", moduleName);
+//     const module = require(prefix + "/package.json").module;
+//     const filePath = path.join(prefix, module);
+//     const ret = fs.readFileSync(filePath, "utf8");
+//     ctx.type = "text/javascript";
+//     ctx.body = rewriteImport(ret);
+//   } else if (url.endsWith(".vue")) {
+//     // 解析单文件组件相当于vue-loader做的事情
+//     // 转换script部分：将默认导出的组件对象转换为常量
+//     const p = path.resolve(__dirname, url.slice(1));
+//     const ret = parse(fs.readFileSync(p, "utf-8"));
+//     const scriptContent = ret.descriptor.script.content;
+//     const script = scriptContent.replace(
+//       "export default ",
+//       "const __script = "
+//     );
+
+//     // 转换template为模板请求
+//     // 将转换获得的渲染函数设置到__script上
+//     // 最后重新导出__script
+//     ctx.type = "text/javascript";
+
+//     //  如果我在这一步就对 url 做解析会怎么样？
+//     // 因为使用 compile 得到的 render 并不是只有一个 render 函数，还有其中的导入依赖的代码逻辑
+//     // 不方便直接拼到 __script.render = 上，  所以在利用一个请求单独来处理
+
+//     ctx.body = `
+//       ${rewriteImport(script)}
+//       import { render as __render } from '${url}?type=template'
+//       __script.render = __render
+//       export default __script
+//     `;
+//   } else if (url.endsWith("?type=template")) {
+//     // 模板编译请求
+//     const p = path.resolve(__dirname, url.split("?")[0].slice(1));
+//     const ret = parse(fs.readFileSync(p, "utf-8"));
+//     const template = ret.descriptor.template.content;
+//     // 使用编译该模板
+//     const render = compile(template, { mode: "module" }).code;
+//     ctx.type = "text/javascript";
+//     ctx.body = rewriteImport(render);
+//   } else if (url.endsWith(".png")) {
+//     ctx.body = fs.readFileSync("src" + url);
+//   }
+// })
+
+app.listen(8080, (ctx) => {
+  console.log("8080");
+});
